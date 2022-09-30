4dcab4b97c59cfe054c91c24e41a0f89fa7aba0a
Naif <naifhagatulah@hotmail.com>
2022-09-30 14:09:20 +0200

    Uppdaterad time4timer funkar nu

---
+++ Naif/Ovning.c
@@ -0,0 +1,50 @@
+#include <pic32mx.h>
+void f()
+{
+
+    volatile int *buttons = (volatile int *)0x8000abc;
+    volatile int *port = (volatile int *)0x8007bc0;
+
+    while (1)
+    {
+        if ((1 << 3) & *buttons)
+        {
+            *port |= 0b111111; // or så vi kommer inte ändra de andra
+        }
+        else
+        {
+            *port &= ~0b111111; // and vi kommer nolla endast de som vi är inreserade av
+        }
+    }
+}
+void q3()
+{
+    volatile int *trisse = (volatile int *)0xbf886100;
+    *(trisse + 1) = 0b1110;
+    TRISECLR = 0b1110;
+    PORTE = (PORTE & ~0b111111) | 0b1010;
+}
+
+// T2CON har set invers och cler använd!!
+void q4()
+{
+    // prescale till 1:64
+    T2CON = 0b110 << 4; // index 4 använd inte set /clr
+    PR2 = (8000000 / 64) / 100;
+    TMR2 = 0;
+    IECSET(0) = 1 << 8;  // sätt 1 på index 8 på IEC(0) enabel interupt på tmt2
+    IPCSET(0) = 0b11111; // sätter priorety och subprio, prioritet handlar om vi får en interupt o är i våra handler och får en ny interupt och den har högre prio har den lägre prio sker de inte
+    enable_interupt();   // startar interupts för våra program
+    T2CONSET = 1 << 15;  // använd set/clr
+}
+
+void q4b()
+{
+    // if(TMR2 == PR2) //funkar ej pga timer tickar för snabbt vi har en interupt i en register
+    if (IFS(0) & (1 << 8))
+    {
+        IFSCLR(0) = 1 << 8; // bara clear drf använd clear o inte direkt noll
+    }
+}
+
+// MÅSTE SLÅ PÅ INTERUPT!!!
--- Naif/time4timer/mipslabwork.c
+++ Naif/time4timer/mipslabwork.c
@@ -13,7 +13,8 @@
 #include <stdint.h>  /* Declarations of uint_32 and the like */
 #include <pic32mx.h> /* Declarations of system-specific addresses etc */
 #include "mipslab.h"
-
+int a;
+int counter;
 int mytime = 0x5957;
 
 char textstring[] = "text, more text, and even more text!";
@@ -38,30 +39,37 @@ void labinit(void)
   Timer 1 prescaler: 1,8,64,256
 
   */
-  volatile int *trise = (volatile int *)0xbf886100; // make a pointer to adress were led are initilized 0 for output we need zero
 
-  *trise = 0; // make sure that the leds are set as output (0)
-  //*prote = 0;
-  TRISD |= 0x00000ff0; // make sure that btns/sw are input leave the others same
+  // PR2 = 9000; // de blir 1 sek om de körs 16 miljoner ggr
+
+  T2CON = 0b111 << 4;     // sätter prescalree till 256
+  PR2 = 99900;            // sätter period
+  TMR2 = 0;               // nollar timer 2
+  IECSET(0) = 0x00000100; // sätter så interupt är enable på timer 2
+  IPCSET(0) = 0b11111;    // sätter prioritet
+  T2CONSET = 0x8000;      // aktiverar timer 2
+
+  volatile int *trise = (volatile int *)0xbf886100; // make a pointer to adress were led are initilized 0 for output we need zero
+  *trise = 0;                                       // make sure that the leds are set as output (0)
+
+  TRISDSET = 0x0ff0; // make sure that btns/sw are input leave the others same
+
+  counter = 0;
   return;
 }
 
 /* This function is called repetitively from the main program */
 void labwork(void)
 {
+
   volatile int *p_led = (volatile int *)0xbf886110;
   // delay(10);
   time2string(textstring, mytime);
   display_string(3, textstring);
-  display_update();
-  tick(&mytime);
 
-  int a = 0;
-  // TMR2 = 0;
-  unsigned int count = TMR2; // så den räknar från 0 till 62499
-  PR2 = 62499;               // de blir 1 sek om de körs 16 miljoner ggr
-  // T2CON = 0xF0;          // prescaler till 256 så de räknar varje gång de körs 256 ggr
-  *p_led = *p_led + 0x1; // lägger till 1 dit led pekar dvs ökar så en till lampa lyser index 0-7 är lampor
+  // så den räknar från 0 till 62499
+  // prescaler till 256 så de räknar varje gång de körs 256 ggr
+
   if (*p_led & 0xffff00) // if we are abow index 7 we dont know what happens if we switch between 0-1
   {
     *p_led = 0x00; // reset incase we come abow index 7
@@ -69,8 +77,6 @@ void labwork(void)
 
   if (getbtns() & 0x02) // kontrollerar om någon av de tre knapparna trycks
   {
-    // timer = 200;
-    // mytime = 0x1111; //ettar allt
     mytime &= 0xff0f;         // nollar minuterna
     mytime |= (getsw()) << 4; // sätter in getsw() in i minuterna allt annat förblir densamma då de bara är nollor
   }
@@ -85,13 +91,20 @@ void labwork(void)
     mytime |= (getsw()) << 12;
   }
   display_image(96, icon);
-  while (1)
+
+  if (IFS(0)) //& 0x080
   {
-    if (count == 62498)
-    {
-      break;
-    }
+    IFSCLR(0) = 0xFFFFFFFF; // clear interupt måste cleara alla vet ej varför? ska bara vara en bit egentlien
+
+    counter++;
+    display_update();
+  }
+  if (counter == 30)
+  {
+    counter = 0;
+    tick(&mytime);
+    *p_led = *p_led + 0x1; // lägger till 1 dit led pekar dvs ökar så en till lampa lyser index 0-7 är lampor
   }
 
-  //  delay(3);
+  // delay(10);
 }
